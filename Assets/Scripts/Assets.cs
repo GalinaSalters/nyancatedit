2bc32e5be4d8035cae55711aed1b6b02fc804fcd
GalinaSalters <152682665+GalinaSalters@users.noreply.github.com>
2023-12-02 10:26:52 +0300

    Initial project commit

---
+++ Assets/Scripts/Assets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Assets : MonoBehaviour
+{
+    private int currentAge = 30;
+    public int addedAge = 1;
+
+    void Start()
+    {
+        ComputeAge();
+        UnityEngine.Debug.Log($"A string can have variables like interted directly!");
+    }
+
+    void ComputeAge()
+    {
+        int totalAge = currentAge + addedAge;
+
+        // Используйте полное имя пространства имен для Debug
+        UnityEngine.Debug.Log("Общий возраст: " + totalAge);
+    }
+}
